c2ea4806316ffb012c273fdbda3b23725c5b5281
Sven Kilberg <sven.kilberg@gmail.com>
2023-02-22 08:20:01 +0100

    Swagger added.

--- Chirper/Chirper/Startup.cs
+++ Chirper/Chirper/Startup.cs
@@ -32,6 +32,9 @@ namespace Chirper
             {
                 configuration.RootPath = "ClientApp/build";
             });
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -48,6 +51,12 @@ namespace Chirper
 
             app.UseStaticFiles();
             app.UseSpaStaticFiles();
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.)
+            app.UseSwaggerUI();
 
             app.UseRouting();
 
